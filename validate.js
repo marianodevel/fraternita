a351d147925ddba7f67b2ffa832e1a4f6ece5822
mariano <marianodevel@pm.me>
2023-10-26 11:34:18 -0300

    deployed

--- validate.js
+++ validate.js
@@ -1,4 +1,4 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
+document.getElementById("contact-form").addEventListener("submit", function (event) {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
